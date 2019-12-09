9488bf83efb863b43c1819a1b1379f722d06517f
JareBear <jaredgrozz@gmail.com>
2019-12-09 09:40:01 -0800

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,34 +1,32 @@
-import smtplib
+import smtplib, os
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from datetime import datetime
 def email_picture(name):
-    # Define these once; use them twice!
-    strFrom = FROM
-    strTo = TO
+    EMAIL = 'xxxxxxxxxxxxxx@gmail.com'
+    EMAIL_TO = 'yyyyyyyyyyyy@gmail.com'
+    EMAIL_PASSWORD = YOUR_EMAIL_PASSWORD
+    strFrom = EMAIL
+    strTo = EMAIL_TO
 
     # Create the root message and fill in the from, to, and subject headers
     msgRoot = MIMEMultipart('related')
-    msgRoot['Subject'] = 'ALERT!'
-    msgRoot['From'] = FROM
-    msgRoot['To'] = TO
+    msgRoot['Subject'] = 'Jared Room ALERT!'
+    msgRoot['From'] = EMAIL
+    msgRoot['To'] = EMAIL_TO
     msgRoot.preamble = 'This is a multi-part message in MIME format.'
 
     # Encapsulate the plain and HTML versions of the message body in an
     # 'alternative' part, so message agents can decide which they want to display.
     msgAlternative = MIMEMultipart('alternative')
     msgRoot.attach(msgAlternative)
-
-    msgText = MIMEText('This is the alternative plain text message.')
-    msgAlternative.attach(msgText)
-
     # We reference the image in the IMG SRC attribute by the ID we give it below
     msgText = MIMEText(f'{datetime.now()}\n<br><img src="cid:image1"><br>', 'html')
     msgAlternative.attach(msgText)
 
     # This example assumes the image is in the current directory
-    fp = open(f'C:/Users/Jared/Documents/Python Scripts/Face Detection/Pics/{name}', 'rb')
+    fp = open(f'{os.path.dirname(os.path.realpath(__file__))}/Pics/{name}', 'rb')
     msgImage = MIMEImage(fp.read())
     fp.close()
 

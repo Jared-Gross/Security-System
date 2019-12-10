8ecb01bba8e9175575730b597b01259e8ea9de5b
JareBear <jaredgrozz@gmail.com>
2019-12-10 06:11:05 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-import cv2, mss, os, os.path, shutil, time, logging, threading, time, sys
+import cv2, mss, os, os.path, shutil, time, logging, threading, time, sys, atexit
 from multiprocessing import Process
 from os.path import isfile, join
 from PIL import Image, ImageGrab
-import moviepy.editor as mp
+import moviepy.editor as mpq
 from datetime import datetime
 import natsort as nt
 from datetime import datetime
 from send_email import email_picture
 from threading import Timer
-
 # captureScreen = IntVar()
 captureScreen = False
 record_video = False
-email_pictures = False
+email_pictures = True
+
+SMILEY_FACE = False
 
 canvas_height=200
 canvas_width=200
@@ -20,14 +21,14 @@ START_TIME = 5
 SEND_EMAIL_DELAY = 10
 TIME = 0
 EMAIL_TIME = 0
-cascadeLocationFolder = ""
+cascadeLocationFolder = "Face Detection/"
 cascadeLists = [
     "{0}haarcascade_frontalface_default.xml".format(cascadeLocationFolder),
     "{0}haarcascade_eye.xml".format(cascadeLocationFolder),
     "{0}haarcascade_fullbody.xml".format(cascadeLocationFolder)
 ]
 selectedCascade = ""
-image_folder = 'Pics'
+image_folder = 'Face Detection/Pics'
 video_name = 'temp.avi'
 numOfPics = 0
 cascade = cv2.CascadeClassifier(cascadeLists[0])
@@ -35,12 +36,6 @@ cap = cv2.VideoCapture(0)
 vid_cod = cv2.VideoWriter_fourcc(*'XVID')
 output = cv2.VideoWriter("cam_video.mp4", vid_cod, 17.0, (640,480))
 TIME_LIMIT = 100
-def on_exists(fname):
-    if os.path.isfile(fname):
-        newfile = fname + ".old"
-        print("{} -> {}".format(fname, newfile))
-        # os.rename(fname, newfile)
-
 def removePictures():
     for the_file in os.listdir(image_folder):
         file_path = os.path.join(image_folder, the_file)
@@ -50,13 +45,7 @@ def removePictures():
             # elif os.path.isdir(file_path): shutil.rmtree(file_path)
         except Exception as e:
             print(e)
-
 def makeVideo():
-        # for index, filename in enumerate(sorted(os.listdir("Face Detection\\Pics"))):
-        #     print ('{0:02d}. {1}'.format(index + 1, filename))
-        # for filename in sorted(os.listdir("Face Detection\\Pics")):
-        #     print (filename)
-
     images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
     if len(images) == 0:
         print('No Images Found')
@@ -73,12 +62,35 @@ def makeVideo():
 def savePicture(num):
     if not captureScreen:
         # CAPTURE WEBCAM
-        return_value,image = cap.read()
-        cv2.imwrite("Pics/{0}.png".format(num),image)
+        return_value, image = cap.read()
+        cv2.imwrite("Face Detection/Pics/{0}.png".format(num),image)
     else:
         # CAPTURE SCREEN
         with mss.mss() as sct:
-            filename = sct.shot(output=f'Pics/{num}.png')
+            filename = sct.shot(output=f'Face Detection/Pics/{num}.png')
+def findFaceInImage(num):
+    cropped_image_name = 'Cropped Image.png'
+    image_name = 'Image.png'
+    img = cv2.imread(f'Face Detection/Pics/{num}.png')
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+    print ("Found {0} faces!".format(len(faces)))
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    img_cropped = cv2.imread(f'Face Detection/Pics/{num}.png')
+    crop_img = img_cropped[y:y+h, x:x+w]
+    cv2.imwrite(f'Face Detection/Pics/{image_name}', img)
+    cv2.imwrite(f'Face Detection/Pics/{cropped_image_name}', crop_img)
+    threading.Thread(target=email_picture, args=([image_name, cropped_image_name],)).start()
+
 def btnStop():
     if record_video:
         makeVideo()
@@ -95,8 +107,6 @@ def btnStop():
     cv2.destroyAllWindows()
     print('closed all windows')
     sys.exit()
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 160)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)
 def camRun():
     print("Starting..")
     global TIME, EMAIL_TIME
@@ -110,21 +120,24 @@ def camRun():
             # Detect faces in the image
             faces = cascade.detectMultiScale(
                 gray,
-                scaleFactor=1.1,
+                scaleFactor=1.2,
                 minNeighbors=5,
                 minSize=(30, 30)
             )
             # Draw a rectangle around the faces
             for (x, y, w, h) in faces:
-                d = int(w / 2)
-                # HEADd
-                cv2.circle(frame, (int(x + w / 2), int(y + h / 2)), d, (d, int(d / 2), int(w)), 2)
-                # LEFT EYE
-                cv2.circle(frame, (int(x + w / 4) + 5, int(y + h / 4) + 15), int(d / 4), (d, int(d / 2), int(w)), 2)
-                # RIGHT EYE
-                cv2.circle(frame, (int(x + w / 1.5) + 5, int(y + h / 4) + 15), int(d / 4), (d, int(d / 2), int(w)), 2)
-                # LIPS
-                cv2.ellipse(frame, (int(x + w / 2), int(y + h / 1.9)), (int(w / 3), int(w / 3)), 0, 25, 155, (d, int(d / 2), int(w)), 2)
+                if SMILEY_FACE:
+                    d = int(w / 2)
+                    # HEAD
+                    cv2.circle(frame, (int(x + w / 2), int(y + h / 2)), d, (int(x), int(d / 2), int(w)), 2)
+                    # LEFT EYE
+                    cv2.circle(frame, (int(x + w / 4) + 5, int(y + h / 4) + 15), int(d / 4), (int(x), int(d / 2), int(w)), 2)
+                    # RIGHT EYE
+                    cv2.circle(frame, (int(x + w / 1.5) + 5, int(y + h / 4) + 15), int(d / 4), (int(x), int(d / 2), int(w)), 2)
+                    # LIPS
+                    cv2.ellipse(frame, (int(x + w / 2), int(y + h / 1.9)), (int(w / 3), int(w / 3)), 0, 25, 155, (int(x), int(d / 2), int(w)), 2)
+                else:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
             # Display the resulting frame
             cv2.imshow('Camera', frame)
             output.write(frame)
@@ -140,14 +153,21 @@ def camRun():
                 savePicture(numOfPics)
             if EMAIL_TIME >= SEND_EMAIL_DELAY and len(faces) >= 1 and not record_video and email_pictures:
                 numOfPics += 1
-                savePicture(numOfPics)
-                email_picture(str(numOfPics) + '.png')
                 EMAIL_TIME = 0
-                print(f'Found {len(faces)} faces!')
+                print(f'Found {len(faces)} faces! Pictures taken: {numOfPics}')
+                savePicture(numOfPics)
+                PROCESS_IMAGE = threading.Thread(target=findFaceInImage, args=(numOfPics,))
+                PROCESS_IMAGE.start()
+                # WAIT FOR PROCCESS TO FINISH:
+                # PROCESS_IMAGE.join()
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             btnStop()
+def exit_handler():
+    print('Closing.. please wait.')
+    btnStop()
 
 if __name__ == '__main__':
-    Process(target=camRun).start()
-    # camRun()
+    atexit.register(exit_handler)
+    # START_CAMERA = threading.Thread(target=camRun).start()
+    START_CAMERA = Process(target=camRun).start()

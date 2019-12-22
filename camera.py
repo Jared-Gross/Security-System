758f7ae4337793b0734e7af6ee2e8c730f566362
JareBear <jaredgrozz@gmail.com>
2019-12-22 12:14:36 -0800

    Update camera.py

--- camera.py
+++ camera.py
@@ -9,6 +9,7 @@ import natsort as nt
 from datetime import datetime
 from send_email import email_picture
 from threading import Timer
+import main
 # captureScreen = IntVar()
 captureScreen = False
 recording = False
@@ -46,12 +47,15 @@ picture_delay = []
 selected_data_index = []
 
 red, green, blue = 0, 0, 0
-
+frame = ''
 image_folder = 'Face Detection/Pics'
 video_name = os.path.dirname(os.path.realpath(__file__)) + '/temp.avi'
 numOfPics = 0
 cascade = cv2.CascadeClassifier(cascade_files[0])
 cap = cv2.VideoCapture(0)
+frame = []
+ret = []
+firstFrame = None
 vid_cod = cv2.VideoWriter_fourcc(*'XVID')
 output = cv2.VideoWriter(os.path.dirname(os.path.realpath(__file__)) + "/cam_video.mp4", vid_cod, 17, (640,480))
 TIME_LIMIT = 100
@@ -206,11 +210,12 @@ def btnStop():
     cv2.destroyAllWindows()
     # sys.exit()
 def camRun():
+    global isRunning, cap, frame, ret, firstFrame
     global TIME, EMAIL_TIME, numOfPics, captureScreen, recording, email_pictures, SMILEY_FACE, SEND_EMAIL_DELAY, START_TIME, cascade, faceDetection
     global saved_color, send_email, cap_screen, record_video, smiley_face, dark_mode, email_delay, picture_delay, saved_color, settings_json, selected_data_index, red, green, blue, face_detect
-    cap = cv2.VideoCapture(0)
-    print("Starting..")
-    threading.Thread(target = update_variables).start()
+    isRunning = True
+    # cap = cv2.VideoCapture(0)
+    # print("Starting..")
     eye_cascade = cv2.CascadeClassifier(cascade_files[0])
     mouth_cascade = cv2.CascadeClassifier(cascade_files[17])
     eyes = []
@@ -220,119 +225,123 @@ def camRun():
     ap.add_argument("-v", "--video", help="path to the video file")
     ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
     args = vars(ap.parse_args())
-    firstFrame = None
-    while isRunning:
-        ret, frame = cap.read()
-        if faceDetection:
-            # Capture frame-by-frame
-            text = "No face/s detected"
-            if ret:
-                # Our operations on the frame come here
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                # Detect faces in the image
-                faces = cascade.detectMultiScale(
-                    gray,
-                    scaleFactor=1.1,
-                    minNeighbors=5,
-                    minSize=(30, 30)
-                )
-                # Draw a rectangle around the faces
-                for (x, y, w, h) in faces:
-                    text = f"Found {len(faces)} faces"
-                    if SMILEY_FACE:
-                        d = int(w / 2)
-                        # HEAD
-                        cv2.circle(frame, (int(x + w / 2), int(y + h / 2)), d, (red, green, blue), 2)
-                        # EYES
-                        roi_gray_mouth = gray[y+(int(h/2)):y+h, x:x+w]
-                        roi_color_mouth = frame[y+(int(h/2)):y+h, x:x+w]
-                        roi_gray_eye = gray[y-(int(h/2)):y+h, x:x+w]
-                        roi_color_eye = frame[y-(int(h/2)):y+h, x:x+w]
-                        eyes = eye_cascade.detectMultiScale(roi_gray_eye)
-                        mouth = mouth_cascade.detectMultiScale(roi_gray_mouth)
-                        for (ex,ey,ew,eh) in eyes:
-                            d = int(ew / 2)
-                            cv2.circle(roi_color_eye, (int(ex + ew / 4) + int(d / 2), int(ey + eh / 4) + int(d / 2)), int(d) ,(blue,green,red),2)
-                        for (ex,ey,ew,eh) in mouth:
-                            # LIPS
-                            cv2.ellipse(roi_color_mouth, (int(ex + ew / 2), int(ey + eh / 8)), (int(ex), int(ey)), 0, 25, 155, (blue,green,red), 2)
-                    else:
-                        cv2.rectangle(frame, (x, y), (x+w, y+h), (red, green, blue), 2)
-                output.write(frame)
-                if len(faces) >= 1:
-                    TIME = START_TIME
-                TIME -= 0.2
-                EMAIL_TIME += 0.2
-
-                if TIME >= 0 and recording:
-                    numOfPics += 1
-                    savePicture(numOfPics)
-                if EMAIL_TIME >= SEND_EMAIL_DELAY and len(faces) >= 1 and not recording and email_pictures:
-                    numOfPics += 1
-                    EMAIL_TIME = 0
-                    print(f'Found {len(faces)} faces! Pictures taken: {numOfPics}')
-                    savePicture(numOfPics)
-                    threading.Thread(target=findFaceInImage, args=(numOfPics,)).start()
-                cv2.putText(frame, datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (red, green, blue), 1)
-            if SMILEY_FACE:
-                cv2.putText(frame, f"Found {len(faces)} face/s!  Found {len(eyes)} eye/s!  Found {len(mouth)} mouth/s!", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (red, green, blue), 1)
-            else:
-                cv2.putText(frame, "{}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (red, green, blue), 2)
-        else:
-            text = "No Movement"
-            if frame is None:
-                break
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            gray = cv2.GaussianBlur(gray, (7, 7), 0)
-            if firstFrame is None:
-                firstFrame = gray
-                continue
-            frameDelta = cv2.absdiff(firstFrame, gray)
-            thresh = cv2.threshold(frameDelta, 7, 255, cv2.THRESH_BINARY)[1]
-            thresh = cv2.dilate(thresh, None, iterations=4)
-            cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            cnts = imutils.grab_contours(cnts)
-            for c in cnts:
-                if cv2.contourArea(c) < args["min_area"]:
-                    continue
-                (x, y, w, h) = cv2.boundingRect(c)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (red, green, blue), 2)
-                text = "Movement"
-                if len(cnts) >= 1:
-                    TIME = START_TIME
-                TIME -= 0.2
-                EMAIL_TIME += 0.2
-
-                if TIME >= 0 and recording:
-                    numOfPics += 1
-                    savePicture(numOfPics)
-                if EMAIL_TIME >= SEND_EMAIL_DELAY and len(cnts) >= 1 and not recording and email_pictures:
-                    numOfPics += 1
-                    EMAIL_TIME = 0
-                    print(f'Found {len(cnts)} faces! Pictures taken: {numOfPics}')
-                    savePicture(numOfPics)
-                    threading.Thread(target=findFaceInImage, args=(numOfPics,)).start()
-            cv2.putText(frame, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (red, green, blue), 2)
-            cv2.putText(frame, datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (red, green, blue), 1)
-            # show the frame and record if the user presses a key
-            # cv2.imshow("Thresh", thresh)
-            # cv2.imshow("Frame Delta", frameDelta)
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord("q"):
-                break
-            firstFrame = gray
-            time.sleep(0.03)
+    # while True:
+    ret, frame = cap.read()
+    if faceDetection and isRunning:
+        # Capture frame-by-frame
+        text = "No face/s detected"
         if ret:
-            cv2.imshow('Camera', frame)
-        if isRunning == False:
-            btnStop()
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            btnStop()
+            # Our operations on the frame come here
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # Detect faces in the image
+            faces = cascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30)
+            )
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in faces:
+                text = f"Found {len(faces)} faces"
+                if SMILEY_FACE:
+                    d = int(w / 2)
+                    # HEAD
+                    cv2.circle(frame, (int(x + w / 2), int(y + h / 2)), d, (red, green, blue), 2)
+                    # EYES
+                    roi_gray_mouth = gray[y+(int(h/2)):y+h, x:x+w]
+                    roi_color_mouth = frame[y+(int(h/2)):y+h, x:x+w]
+                    roi_gray_eye = gray[y-(int(h/2)):y+h, x:x+w]
+                    roi_color_eye = frame[y-(int(h/2)):y+h, x:x+w]
+                    eyes = eye_cascade.detectMultiScale(roi_gray_eye)
+                    mouth = mouth_cascade.detectMultiScale(roi_gray_mouth)
+                    for (ex,ey,ew,eh) in eyes:
+                        d = int(ew / 2)
+                        cv2.circle(roi_color_eye, (int(ex + ew / 4) + int(d / 2), int(ey + eh / 4) + int(d / 2)), int(d) ,(blue,green,red),2)
+                    for (ex,ey,ew,eh) in mouth:
+                        # LIPS
+                        cv2.ellipse(roi_color_mouth, (int(ex + ew / 2), int(ey + eh / 8)), (int(ex), int(ey)), 0, 25, 155, (blue,green,red), 2)
+                else:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (red, green, blue), 2)
+            output.write(frame)
+            if len(faces) >= 1:
+                TIME = START_TIME
+            TIME -= 0.2
+            EMAIL_TIME += 0.2
+
+            if TIME >= 0 and recording:
+                numOfPics += 1
+                savePicture(numOfPics)
+            if EMAIL_TIME >= SEND_EMAIL_DELAY and len(faces) >= 1 and not recording and email_pictures:
+                numOfPics += 1
+                EMAIL_TIME = 0
+                print(f'Found {len(faces)} faces! Pictures taken: {numOfPics}')
+                savePicture(numOfPics)
+                threading.Thread(target=findFaceInImage, args=(numOfPics,)).start()
+            cv2.putText(frame, datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (red, green, blue), 1)
+        if SMILEY_FACE:
+            cv2.putText(frame, f"Found {len(faces)} face/s!  Found {len(eyes)} eye/s!  Found {len(mouth)} mouth/s!", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (red, green, blue), 1)
+        else:
+            cv2.putText(frame, "{}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (red, green, blue), 2)
+    else:
+        text = "No Movement"
+        if frame is None:
+            # break
+            return
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (7, 7), 0)
+        if firstFrame is None:
+            firstFrame = gray
+            # continue
+        frameDelta = cv2.absdiff(firstFrame, gray)
+        thresh = cv2.threshold(frameDelta, 7, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.dilate(thresh, None, iterations=4)
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        for c in cnts:
+            if cv2.contourArea(c) < args["min_area"]:
+                continue
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (red, green, blue), 2)
+            text = "Movement"
+            if len(cnts) >= 1:
+                TIME = START_TIME
+            TIME -= 0.2
+            EMAIL_TIME += 0.2
+
+            if TIME >= 0 and recording:
+                numOfPics += 1
+                savePicture(numOfPics)
+            if EMAIL_TIME >= SEND_EMAIL_DELAY and len(cnts) >= 1 and not recording and email_pictures:
+                numOfPics += 1
+                EMAIL_TIME = 0
+                print(f'Found {len(cnts)} faces! Pictures taken: {numOfPics}')
+                savePicture(numOfPics)
+                threading.Thread(target=findFaceInImage, args=(numOfPics,)).start()
+        cv2.putText(frame, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (red, green, blue), 2)
+        cv2.putText(frame, datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (red, green, blue), 1)
+        # show the frame and record if the user presses a key
+        # cv2.imshow("Thresh", thresh)
+        # cv2.imshow("Frame Delta", frameDelta)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            # break
+            return
+        firstFrame = gray
+        time.sleep(0.03)
+    if isRunning == False:
+        btnStop()
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        btnStop()
+    if ret:
+        return ret, frame
+        # cv2.imshow('Camera', frame)
+        # main.frame = frame
+        # main.rets = ret
 def exit_handler():
     print('Closing.. please wait.')
     btnStop()
 def update_variables():
-    while isRunning:
+    while True:
         global TIME, EMAIL_TIME, numOfPics, captureScreen, recording, email_pictures, SMILEY_FACE, SEND_EMAIL_DELAY, START_TIME, cascade, faceDetection
         global saved_color, send_email, cap_screen, record_video, smiley_face, dark_mode, email_delay, picture_delay, saved_color, settings_json, selected_data_index, red, green, blue, face_detect
         saved_color.clear()
@@ -382,7 +391,8 @@ def start_cam():
     global isRunning
     isRunning = True
     atexit.register(exit_handler)
-    threading.Thread(target=camRun).start()
+    threading.Thread(target = update_variables).start()
+    # threading.Thread(target=camRun).start()
     # camRun()
 def end_cam():
     global isRunning

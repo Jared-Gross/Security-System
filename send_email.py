642c7f50c16366127e5bcbaaba75ed9f028019e1
JareBear <jaredgrozz@gmail.com>
2020-01-15 11:47:58 -0600

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -7,16 +7,18 @@ from datetime import datetime
 def email_picture(name, text):
     settings_file = os.path.dirname(os.path.realpath(__file__)) + '/settings.json'
     email_address = []
+    ip = []
+    image_folder = 'Pics'
     with open(settings_file) as file:
         settings_json = json.load(file)
         for info in settings_json:
-            for email in info['email address']:
-                email_address.append(email)
+            for email in info['email address']: email_address.append(email)
+            for ipaddress in info['host address']: ip.append(ipaddress)
     EMAIL = 'jaredgrozz@gmail.com'
-    EMAIL_TO = email_address[0]
+    EMAIL_TO = email_address[0].split(', ')
     EMAIL_PASSWORD = 'ffvprugomuuywemq'#<-
     '''                                   \
-                                           \
+                                        \
     DO NOT USE YOUR ACTUAL GMAIL PASSWORD!! \
     go here sign into your google account    \
     https://myaccount.google.com/security     \
@@ -25,44 +27,35 @@ def email_picture(name, text):
     
     Also showcased here: https://www.interviewqs.com/blog/py_email
     '''
-    # Create the root message and fill in the from, to, and subject headers
-    msgRoot = MIMEMultipart('related')
-    msgRoot['Subject'] = 'Jared Room ALERT!'
-    msgRoot['From'] = EMAIL
-    msgRoot['To'] = EMAIL_TO
-    msgRoot.preamble = 'This is a multi-part message in MIME format.'
-
-    # Encapsulate the plain and HTML versions of the message body in an
-    # 'alternative' part, so message agents can decide which they want to display.
-    msgAlternative = MIMEMultipart('alternative')
-    msgRoot.attach(msgAlternative)
-    # We reference the image in the IMG SRC attribute by the ID we give it below
-    msgText = MIMEText(f'{text}\n\n{datetime.now()}\n<br><img src="cid:image1"><img src="cid:image2"><br>', 'html')
-    msgAlternative.attach(msgText)
-
-    # Define the image's ID as referenced above
-    for i, j in enumerate(name):
-        try:
-            with open(f'Pics/{name[i]}', 'rb') as fp:
-                msgImage = MIMEImage(fp.read())
-                msgImage.add_header('Content-ID', f'<image>{i}')
-                msgRoot.attach(msgImage)
-        except:
-            with open(f'Pics/{name[0]}', 'rb') as fp:
-                msgImage = MIMEImage(fp.read())
-                msgImage.add_header('Content-ID', f'<image>{i}')
-                msgRoot.attach(msgImage)
-
-    # Send the email (this example assumes SMTP authentication is required)
-    smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    smtp.login(EMAIL, EMAIL_PASSWORD)
-    smtp.sendmail(EMAIL, EMAIL_TO, msgRoot.as_string())
-    smtp.quit()
-    print('Email sent!')
-    image_folder = 'Pics'
+    for i, k in enumerate(EMAIL_TO):
+        msgRoot = MIMEMultipart('related')
+        msgRoot['Subject'] = 'Carpentar Shop ALERT!'
+        msgRoot['From'] = EMAIL
+        msgRoot['To'] = k
+        msgAlternative = MIMEMultipart('alternative')
+        msgRoot.attach(msgAlternative)
+        msgText = MIMEText(f'{text}\n\n{datetime.now()}\n<br><img src="cid:image1"><img src="cid:image2"><br> <a href=\"http://{ip}:5000\">Watch Live</a>', 'html')
+        msgAlternative.attach(msgText)
+        for i, j in enumerate(name):
+            try:
+                with open(f'Pics/{name[i]}', 'rb') as fp:
+                    msgImage = MIMEImage(fp.read())
+                    msgImage.add_header('Content-ID', f'<image>{i}')
+                    msgRoot.attach(msgImage)
+            except FileNotFoundError:
+                with open(f'Pics/{name[0]}', 'rb') as fp:
+                    msgImage = MIMEImage(fp.read())
+                    msgImage.add_header('Content-ID', f'<image>{i}')
+                    msgRoot.attach(msgImage)
+        smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        smtp.login(EMAIL, EMAIL_PASSWORD)
+        smtp.sendmail(EMAIL, k, msgRoot.as_string())
+        smtp.quit()
+        print('Email sent!')
     for the_file in os.listdir(image_folder):
         file_path = os.path.join(image_folder, the_file)
         try:
-            if os.path.isfile(file_path):  os.unlink(file_path)
-        except Exception as e: print(e)
+            if os.path.isfile(file_path): os.unlink(file_path)
+        except Exception as e:
+            print(e)
     print('Images deleted')
